bfdf8f7a54e3c2179f4c0f5f0086454c36453cf2
akshitchordia <chordiaakshit3@gmail.com>
2025-07-31 10:01:30 +0530

    Create printing_sit.cpp

---
+++ printing_sit.cpp
@@ -0,0 +1,21 @@
+// Name - Akshit Chordia
+// PRN - 24070123008
+// Batch - EnTC - A1
+
+
+#include <iostream>
+using namespace std;
+int main() {
+for(int i=0;i <5;i++){
+    cout<<"SIT"<<endl;
+}     
+      return 0;
+}
+
+/*Output-
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
