fe1d2b95c0a8eb28e2915fb58391e5170c7a3b05
akshitchordia <chordiaakshit3@gmail.com>
2025-07-31 09:59:23 +0530

    Create 0_to_10.cpp

---
+++ 0_to_10.cpp
@@ -0,0 +1,25 @@
+// Name - Akshit Chordia
+// PRN - 24070123008
+// Batch - EnTC - A1
+
+#include <iostream>
+using namespace std;
+int main() {
+for(int i=0;i <=10 ;i++){
+    if(i%2==0){
+      cout<<i<<endl;  
+      i++;
+    }
+}    
+    return 0;
+}
+
+/*
+output:
+0
+2
+4
+6
+8
+10
+*/
