d6ebcdcaa1526c56a466d8f276feeb6190f6908b
akshitchordia <chordiaakshit3@gmail.com>
2025-07-31 10:14:25 +0530

    Create floyd_series.cpp

---
+++ floyd_series.cpp
@@ -0,0 +1,30 @@
+// Name - Akshit Chordia
+// PRN - 24070123008
+// Batch - EnTC - A1
+
+
+#include <iostream>
+using namespace std;
+int i,j;
+int n=4;
+int k=1;
+
+int main(){
+  for(i=1;i<=n;i++){
+      for(j=0;j<i;j++){
+          cout<< k << " ";
+          k++;
+      }
+      cout<<endl;
+  }
+  
+}
+
+
+/*Output:-
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+
+*/
